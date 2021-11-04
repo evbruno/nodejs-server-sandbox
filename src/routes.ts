7195e0af49a906b609017d7fde9972b7ff9da705
Eduardo V. Bruno <eduardo.bruno@fauna.com>
2021-11-04 17:00:24 -0300

    add async-handler for express

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction, Express } from 'express'
 import express from 'express'
 import Logger from './logger'
+import asyncHandler from 'express-async-handler'
 
 let routes = express.Router()
 
@@ -21,11 +22,36 @@ routes.get('/hello/:name?', (req: Request, res: Response) => {
   res.json({hello: req.params['name'] || 'anonymous'})
 })
 
-
 routes.get('/div/:x([0-9]{1,})/:y([0-9]{1,})', (req: Request, res: Response) => {
   Logger.info(`going to divide ${req.params['x']} by ${req.params['y']}`)
   const r = parseInt(req.params['x']) / parseInt(req.params['y'])
   res.json({result: r})
+})
+
+// faking async
+const myAPICall = () => new Promise((resolve, reject) => {
+  let t  = Math.floor(Math.random() * 10) * 10
+  setTimeout(() => {
+    if (t < 60)
+      resolve(`api took ${t} ms...`)
+    else
+      reject(`api is taking too long...`)
+  }, t);
+});
+
+// using express-async-handler
+
+routes.get('/db', asyncHandler(async (req: Request, res: Response) => {
+  Logger.info(`going to call the db, v1`)
+  let msg = await myAPICall()
+  res.json({result: msg})
+}))
+
+routes.get('/db2', async (req: Request, res: Response, next: NextFunction) => {
+  Logger.info(`going to call the db, v2`)
+  myAPICall()
+    .then(msg => res.json({result: msg}))
+    .catch(next)
 })
 
 export default routes
